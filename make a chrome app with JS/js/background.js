9a8a98e1dc6328791bdb5a3aa9e6b6143fae357a
piecejaeyoung <125432018+piecejaeyoung@users.noreply.github.com>
2023-02-22 11:12:31 +0900

    Delete make a chrome app with JS directory

--- make a chrome app with JS/js/background.js
+++
@@ -1,9 +0,0 @@
-const images = ["1.jpg", "2.jpg", "3.jpg"]
-
-const RANDOM1 = Math.random() * images.length;
-const chosenImage = images[Math.floor(RANDOM1)] // = const chosenimage = images[Math.floor(Math.random()*images.length)];
-
-const bgImage = document.createElement("img");
-
-bgImage.src = `img/${chosenImage}`;
-document.body.appendChild(bgImage);
